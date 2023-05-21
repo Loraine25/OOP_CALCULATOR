debcf301e25f0d688a6daef6c5df5d545b549cc4
unknown <anonuevoloraine6@gmail.com>
2023-05-21 19:34:56 +0800

    Greetings

---
+++ Object_Oriented_Prog_Calculator.py
@@ -0,0 +1,14 @@
+# Anonuevo, Loraine N.
+# BSCpE 1-4
+# OOP CALCULATOR
+
+#Header
+print("\033[;33;1;3m\033[10m" * 65)
+print("\033[;33;1;3mHi! It's a pleasure to have you here!\033[0m".center(81))
+print("\033[;33;1;3mಠ\033[10m" * 65)
+
+#Request about the user's name.
+print("")
+print("\033[1;3mMy name is \033[;45;1;3mLoraine\033[0m")
+your_name = input("\033[1;3mWhat is your name?\033[0m")
+print("\033[;1;3mI'm glad that you're here!\033[;34;1;3m" + your_name + "\033[0m \033[;1;3m, sit back and learn with me!\033[0m")
